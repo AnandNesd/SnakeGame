b28649b74e8487e7815e3fc5643c23589a6c5e6e
name <email>
2021-08-13 11:06:05 -0700

    addded index.php and composer.json

--- game.js
+++ game.js
@@ -4,6 +4,14 @@ var requestAnimationFrame =
   window.mozRequestAnimationFrame;
 
 function loop() {
+  if (game.over == false) {
+    game.resetCanvas();
+    game.drawScore();
+    snake.move();
+    food.draw();
+    snake.draw();
+    game.drawMessage();
+  }
   setTimeout(function () {
     requestAnimationFrame(loop);
   }, 1000 / game.fps);
